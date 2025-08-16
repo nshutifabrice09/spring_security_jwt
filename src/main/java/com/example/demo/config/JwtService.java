03fbbe46893ce3fc10a401d8d581bf86d0174008
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-16 12:53:40 +0200

    extractUsername-update-commit

--- src/main/java/com/example/demo/config/JwtService.java
+++ src/main/java/com/example/demo/config/JwtService.java
@@ -1,10 +1,39 @@
 package com.example.demo.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
+
+    private static final String SECRET_KEY = "3BF485D716EF32FEE88A2BFE157ED";
     public String extractUsername(String token) {
         return null;
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
