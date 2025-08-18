0fe11a9b313747cf2ada021c76eb587df4b316a4
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-18 12:01:14 +0200

    initial-SecurityConfig-commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,2 +1,4 @@
-package com.example.demo.config;public class SecurityConfig {
+package com.example.demo.config;
+
+public class SecurityConfig {
 }
--- src/main/java/com/example/demo/exception/UsernameNotFoundException.java
+++ src/main/java/com/example/demo/exception/UsernameNotFoundException.java
@@ -2,7 +2,7 @@ package com.example.demo.exception;
 
 public class UsernameNotFoundException extends RuntimeException{
 
-    public UsernameNotFoundException(String id){
+    public UsernameNotFoundException(Integer id){
         super
                 ("Couldn't find user with id "+id);
     }
