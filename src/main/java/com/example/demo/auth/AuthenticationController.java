d73a80a01833721c1b4f7613534960054f86a52e
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-19 12:00:40 +0200

    RegisterRequest-commit

--- src/main/java/com/example/demo/auth/AuthenticationController.java
+++ src/main/java/com/example/demo/auth/AuthenticationController.java
@@ -1,6 +1,9 @@
 package com.example.demo.auth;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request){
+        //
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody AuthenticationRequest request){
+
+    }
+    //
 }
--- src/main/java/com/example/demo/auth/AuthenticationResponse.java
+++ src/main/java/com/example/demo/auth/AuthenticationResponse.java
@@ -1,2 +1,23 @@
-package com.example.demo.auth;public class AuthenticationResponse {
+package com.example.demo.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
