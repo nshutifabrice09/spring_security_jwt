a6a21dc41ebe1c77ec9f8cf4d3fe88a7ae1b35ad
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2025-08-19 10:57:19 +0200

    updates-applicationConfig

--- src/main/java/com/example/demo/exception/UsernameNotFoundException.java
+++ src/main/java/com/example/demo/exception/UsernameNotFoundException.java
@@ -2,7 +2,7 @@ package com.example.demo.exception;
 
 public class UsernameNotFoundException extends RuntimeException{
 
-    public UsernameNotFoundException(Integer id){
+    public UsernameNotFoundException(String id){
         super
                 ("Couldn't find user with id "+id);
     }
